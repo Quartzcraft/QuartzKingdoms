7baf45e36a8f51010fa391f297d2c616ed606970
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-2.local>
2013-09-02 06:54:48 +1000

    Added QuartzKingdoms.java

--- src/uk/co/quartzcraft/kingdoms/QuartzKingdoms.java
+++ src/uk/co/quartzcraft/kingdoms/QuartzKingdoms.java
@@ -1,5 +1,20 @@
 package uk.co.quartzcraft.kingdoms;
 
-public class QuartzKingdoms {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class QuartzKingdoms extends JavaPlugin {
+
+	public void onEnable(){
+		
+        //Startup notice
+		getLogger().info("The QuartzKingdoms Plugin has been enabled!");
+	}
+	 
+	@Override
+	public void onDisable() {
+    	//Shutdown notice
+		getLogger().info("The QuartzKingdoms Plugin has been disabled!");
+	}
 
 }
