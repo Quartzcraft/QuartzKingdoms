1cc9ff8a370db0d8f5d9f8e34d9acd2b2ca8a233
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-3.local>
2013-12-12 08:21:12 +1000

    Fixed errors in QuartzKingdoms.jar

--- src/uk/co/quartzcraft/kingdoms/QuartzKingdoms.java
+++ src/uk/co/quartzcraft/kingdoms/QuartzKingdoms.java
@@ -7,50 +7,47 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import uk.co.quartzcraft.QuartzCore;
-import uk.co.quartzcraft.Defaults;
-import uk.co.quartzcraft.database.MySQL;
 
-public class QuartzKingdoms extends JavaPlugin implements Defaults {
+public class QuartzKingdoms extends JavaPlugin {
 	
 	public Plugin plugin = this.plugin;
 	
-	public static Connection DBCore = null;
-	public static Connection DBKing = null;
-
-	MySQL MySQL = new MySQL(plugin, "localhost", "3306", "Quartz", "root", "database1");
-	MySQL MySQLking = new MySQL(plugin, "localhost", "3306", "Kingdoms", "root", "database1");
+	public static final Logger log = Logger.getLogger("Minecraft");
 	
+	//public static Connection DBCore = null;
+	//public static Connection DBKing = null;
+
+	//MySQL MySQLcore = new MySQL(plugin, "localhost", "3306", "Quartz", "root", "database1");
+	//MySQL MySQLking = new MySQL(plugin, "localhost", "3306", "Kingdoms", "root", "database1");
+	
+	@Override
 	public void onEnable() {
 		
-		Logger logger = getLogger();
-		
 		//Database
-		logger.info("[STARTUP]Connecting to Database");
-		DBKing = MySQL.openConnection();
-		DBCore = MySQL.openConnection();
+		//logger.info("[STARTUP]Connecting to Database");
+		//DBKing = MySQLking.openConnection();
+		//DBCore = MySQLcore.openConnection();
 		
 		//Listeners
-		logger.info("[STARTUP]Registering Listeners");
+		log.info("[STARTUP]Registering Listeners");
 		
 		//Commands
-		logger.info("[STARTUP]Registering Commands");
+		log.info("[STARTUP]Registering Commands");
 	   	
         //Startup notice
-		logger.info("The QuartzKingdoms Plugin has been enabled!");
-		logger.info("Compiled using QuartzCore version " + release + " " + version);
+		log.info("The QuartzKingdoms Plugin has been enabled!");
+		log.info("Compiled using QuartzCore version " + QuartzCore.release + " " + QuartzCore.version);
 	}
 	 
 	@Override
 	public void onDisable() {
 		
-		Logger logger = getLogger();
-		
 		//Database
-		logger.info("[SHUTDOWN]Terminating Connection to Database");
+		//log.info("[SHUTDOWN]Terminating Connection to Database");
 		//DBKing = MySQL.closeConnection();
 				
     	//Shutdown notice
-		getLogger().info("The QuartzKingdoms Plugin has been disabled!");
+		log.info("The QuartzKingdoms Plugin has been disabled!");
 	}
 
 }
