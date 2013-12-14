077a64da648c2fb3cbbfb586bd1310e02a81dcac
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-4.local>
2013-12-14 14:54:02 +1000

    Moved and changed things, things should work now, sort of >.>

--- src/uk/co/quartzcraft/kingdoms/QuartzKingdoms.java
+++ src/uk/co/quartzcraft/kingdoms/QuartzKingdoms.java
@@ -6,7 +6,8 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import uk.co.quartzcraft.QuartzCore;
+
+import uk.co.quartzcraft.core.QuartzCore;
 
 public class QuartzKingdoms extends JavaPlugin {
 	
@@ -14,11 +15,22 @@ public class QuartzKingdoms extends JavaPlugin {
 	
 	public static final Logger log = Logger.getLogger("Minecraft");
 	
+	public static String releaseVersion = QuartzCore.displayReleaseVersion();
 	//public static Connection DBCore = null;
 	//public static Connection DBKing = null;
 
 	//MySQL MySQLcore = new MySQL(plugin, "localhost", "3306", "Quartz", "root", "database1");
 	//MySQL MySQLking = new MySQL(plugin, "localhost", "3306", "Kingdoms", "root", "database1");
+	
+	@Override
+	public void onDisable() {
+		
+		//Database
+		//log.info("[SHUTDOWN]Terminating Connection to Database");
+		//DBKing = MySQL.closeConnection();
+				
+    	//Shutdown notice
+	}
 	
 	@Override
 	public void onEnable() {
@@ -29,25 +41,12 @@ public class QuartzKingdoms extends JavaPlugin {
 		//DBCore = MySQLcore.openConnection();
 		
 		//Listeners
-		log.info("[STARTUP]Registering Listeners");
+		log.info("[QK][STARTUP]Registering Listeners");
 		
 		//Commands
-		log.info("[STARTUP]Registering Commands");
 	   	
         //Startup notice
-		log.info("The QuartzKingdoms Plugin has been enabled!");
-		log.info("Compiled using QuartzCore version " + QuartzCore.release + " " + QuartzCore.version);
-	}
-	 
-	@Override
-	public void onDisable() {
-		
 		//Database
-		//log.info("[SHUTDOWN]Terminating Connection to Database");
-		//DBKing = MySQL.closeConnection();
-				
-    	//Shutdown notice
-		log.info("The QuartzKingdoms Plugin has been disabled!");
 	}
 
 }
