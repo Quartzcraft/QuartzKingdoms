ceabe4f607dbc7b534328559d5e8f65674bae6ac
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-6.local>
2014-01-19 15:43:21 +1000

    Updated some stuff, still not done

--- src/uk/co/quartzcraft/kingdoms/command/KingdomCreateSubCommand.java
+++ src/uk/co/quartzcraft/kingdoms/command/KingdomCreateSubCommand.java
@@ -1,20 +1,37 @@
 package uk.co.quartzcraft.kingdoms.command;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import uk.co.quartzcraft.core.command.QSubCommand;
+import uk.co.quartzcraft.kingdoms.QuartzKingdoms;
 
 public class KingdomCreateSubCommand extends QSubCommand {
 
 	@Override
 	public String getPermission() {
-		return "QCK.Kingdom";
+		return "QCK.Kingdom.create";
 	}
 
 	@Override
 	public void onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		// TODO Auto-generated method stub
+		
+		if(args[1] != null) {
+			try {
+				Statement s = QuartzKingdoms.MySQLking.openConnection().createStatement();
+				ResultSet res = s.executeQuery("INSERT INTO Kingdoms ();");
+		        res.next();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			
+		}
 		
 	}
 
