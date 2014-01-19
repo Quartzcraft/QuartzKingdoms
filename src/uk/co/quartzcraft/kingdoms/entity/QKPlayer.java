ec10c19630b725afc386088f866765531997ef99
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-6.local>
2014-01-19 15:43:46 +1000

    Created QKPlayer class, still finishing

---
+++ src/uk/co/quartzcraft/kingdoms/entity/QKPlayer.java
@@ -0,0 +1,49 @@
+package uk.co.quartzcraft.kingdoms.entity;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
+
+import uk.co.quartzcraft.core.QuartzCore;
+import uk.co.quartzcraft.core.entity.QPlayer;
+import uk.co.quartzcraft.kingdoms.QuartzKingdoms;
+
+public class QKPlayer extends QPlayer {
+
+	public HashMap getDataThisPlugin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String getKingdom(Player player) {
+		String kingdom = null;
+		
+		String playername = player.toString();
+		
+		try {
+			Statement s1 = QuartzCore.MySQLcore.openConnection().createStatement();
+			Statement s2 = QuartzKingdoms.MySQLking.openConnection().createStatement();
+			
+	        ResultSet res1 = s1.executeQuery("SELECT * FROM PlayerData WHERE DisplayName ='" + playername + "';");
+	        res1.next();
+	        
+	        ResultSet res2 = s2.executeQuery("SELECT * FROM KingdomsPlayerData WHERE playerID =" + res1.getInt(1) + ";");
+	        res2.next();
+	        
+		} catch(SQLException e) {
+			
+		}
+		
+		return kingdom;
+	}
+
+	@Override
+	public boolean createPlayerThisPlugin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
