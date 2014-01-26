56a3117d2d71d594f0acb11e671e0690cb6cec6d
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-6.local>
2014-01-26 11:45:39 +1000

    Created basic kingdom methods - more to come

--- src/uk/co/quartzcraft/kingdoms/kingdom/Kingdom.java
+++ src/uk/co/quartzcraft/kingdoms/kingdom/Kingdom.java
@@ -2,4 +2,18 @@ package uk.co.quartzcraft.kingdoms.kingdom;
 
 public class Kingdom {
 
+	public boolean createKingdom() {
+		return false;
+		
+	}
+	
+	public static boolean deleteKingdom() {
+		return false;
+		
+	}
+	
+	public static String getName() {
+		return null;
+		
+	}
 }
