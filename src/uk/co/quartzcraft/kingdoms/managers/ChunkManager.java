72f1e8226c0df5d7cb67271f6847fd7f5a909a20
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-6.local>
2014-01-26 11:46:07 +1000

    Added some more stuff, probably need to finish Kingdoms first

--- src/uk/co/quartzcraft/kingdoms/managers/ChunkManager.java
+++ src/uk/co/quartzcraft/kingdoms/managers/ChunkManager.java
@@ -6,12 +6,20 @@ import org.bukkit.entity.Player;
 import uk.co.quartzcraft.core.entity.QPlayer;
 import uk.co.quartzcraft.kingdoms.QuartzKingdoms;
 
-
-
 public class ChunkManager {
 
+	public boolean checkClaim(Chunk chunk) {
+		return false;
+	}
+	
 	public void claimChunk(Player chunkClaimer) {
 		Chunk chunk = chunkClaimer.getLocation().getChunk();
+		
+		if(checkClaim(chunk) == false) {
+			//claim chunk
+		} else {
+			//error
+		}
 	}
 	
 	
