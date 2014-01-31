791c42742d4a760457399c69317286bbb942e33d
ZeWildGuy <nicholasbassett@Nicholass-MacBook-Air-6.local>
2014-01-31 19:34:18 +1000

    Changes to Kingdom Info subcommands - still need to add result iteration

--- src/uk/co/quartzcraft/kingdoms/command/KingdomInfoSubCommand.java
+++ src/uk/co/quartzcraft/kingdoms/command/KingdomInfoSubCommand.java
@@ -4,7 +4,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import uk.co.quartzcraft.core.chat.ChatPhrase;
 import uk.co.quartzcraft.core.command.QSubCommand;
+import uk.co.quartzcraft.kingdoms.kingdom.Kingdom;
 
 public class KingdomInfoSubCommand extends QSubCommand {
 
@@ -12,7 +14,12 @@ public class KingdomInfoSubCommand extends QSubCommand {
              return "QCK.Kingdom";
      }
      
-     public void onCommand(CommandSender sender, Command cmd, String label, String[] args) {        
-             sender.sendMessage("Info on Kingdom: ");
+     public void onCommand(CommandSender sender, Command cmd, String label, String[] args) {  
+    	 String arg = args[1];
+         String[] info = Kingdom.getInfo(arg);
+         
+         if(info != null) {
+        	 sender.sendMessage(ChatPhrase.getPhrase("info_kingdom"));
+         }
      }
 }
