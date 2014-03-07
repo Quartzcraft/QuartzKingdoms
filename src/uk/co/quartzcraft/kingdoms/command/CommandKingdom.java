05cbe83f9c2feccab02654b5771a286e60fe4392
ZeWildGuy <njb3112@iCloud.com>
2014-03-08 06:46:46 +1000

    Removing useless imports and swapping messages for phrases

--- src/uk/co/quartzcraft/kingdoms/command/CommandKingdom.java
+++ src/uk/co/quartzcraft/kingdoms/command/CommandKingdom.java
@@ -1,32 +1,20 @@
 package uk.co.quartzcraft.kingdoms.command;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
-import uk.co.quartzcraft.core.QuartzCore;
 import uk.co.quartzcraft.core.chat.ChatPhrase;
 import uk.co.quartzcraft.core.command.QSubCommand;
-import uk.co.quartzcraft.kingdoms.QuartzKingdoms;
 
 public class CommandKingdom implements CommandExecutor {
 	
 	private static HashMap<List<String>, QSubCommand> commands = new HashMap<List<String>, QSubCommand>();
-    
+
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
             if(args.length >= 1) {
                     boolean match = false; 
@@ -46,7 +34,7 @@ public class CommandKingdom implements CommandExecutor {
                     }
             }
             else {
-                    sender.sendMessage(ChatColor.GREEN + "Please specify a SubCommand. Visit the QuartzCraft Wiki for help.");
+                    sender.sendMessage(ChatPhrase.getPhrase("Specify_SubCommand"));
             }
             
             return true;
