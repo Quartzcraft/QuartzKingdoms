177f77528101b09ed30741bce8d98074593f3c4a
ZeWildGuy <njb3112@iCloud.com>
2014-03-13 07:31:00 +1000

    Created PlayerDeath and PlayerRespawn listeners

---
+++ src/uk/co/quartzcraft/kingdoms/listeners/PlayerDeathListener.java
@@ -0,0 +1,36 @@
+package uk.co.quartzcraft.kingdoms.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import uk.co.quartzcraft.core.chat.ChatPhrase;
+import uk.co.quartzcraft.core.event.QPlayerCreationEvent;
+import uk.co.quartzcraft.kingdoms.QuartzKingdoms;
+import uk.co.quartzcraft.kingdoms.entity.QKPlayer;
+
+public class PlayerDeathListener implements Listener {
+
+    private static QuartzKingdoms plugin;
+
+    public PlayerDeathListener(QuartzKingdoms plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        this.plugin = plugin;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        Player player = e.getEntity();
+        //QKPlayer.setPower(player, false, 5);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerRespawn(PlayerRespawnEvent e) {
+        Player player = e.getPlayer();
+        QKPlayer.setPower(player, false, 5);
+    }
+
+}
