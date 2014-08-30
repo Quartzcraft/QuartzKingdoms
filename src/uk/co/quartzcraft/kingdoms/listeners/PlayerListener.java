70cc0a5254119332753213c793031137e3b00502
ZeWildGuy <njb3112@iCloud.com>
2014-08-30 18:09:26 +1000

    More efficient Async running of player move event

--- src/uk/co/quartzcraft/kingdoms/listeners/PlayerListener.java
+++ src/uk/co/quartzcraft/kingdoms/listeners/PlayerListener.java
@@ -1,5 +1,6 @@
 package uk.co.quartzcraft.kingdoms.listeners;
 
+import org.bukkit.Chunk;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,6 +13,7 @@ import uk.co.quartzcraft.core.data.QPlayer;
 import uk.co.quartzcraft.core.event.QPlayerCreationEvent;
 import uk.co.quartzcraft.core.event.QPlayerJoinEvent;
 import uk.co.quartzcraft.core.systems.chat.QCChat;
+import uk.co.quartzcraft.core.util.TaskChain;
 import uk.co.quartzcraft.kingdoms.QuartzKingdoms;
 import uk.co.quartzcraft.kingdoms.data.QKPlayer;
 import uk.co.quartzcraft.kingdoms.systems.landclaim.ChunkManager;
@@ -66,25 +68,35 @@ public class PlayerListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-        if (event.getFrom().getChunk().equals(event.getTo().getChunk())) {
-            //Nothing
-        } else {
-            if(ChunkManager.isClaimed(event.getTo().getChunk()) && ChunkManager.isClaimed(event.getFrom().getChunk())) {
-                if(!ChunkManager.getKingdomOwner(event.getFrom().getChunk()).equals(ChunkManager.getKingdomOwner(event.getTo().getChunk()))) {
-                    player.sendMessage(QCChat.getPhrase("now_leaving_the_land_of") + ChunkManager.getKingdomOwner(event.getFrom().getChunk()));
-                    player.sendMessage(QCChat.getPhrase("now_entering_the_land_of") + ChunkManager.getKingdomOwner(event.getTo().getChunk()));
-                } else {
-                    //player.sendMessage(QCChat.getPhrase("now_entering_the_land_of") + ChunkManager.getKingdom(event.getTo().getChunk()));
+        final Player player = event.getPlayer();
+        final Chunk chunkFrom = event.getFrom().getChunk();
+        final Chunk chunkTo = event.getTo().getChunk();
+
+        TaskChain.newChain().add(new TaskChain.AsyncGenericTask() {
+            @Override
+            protected void run() {
+                if(chunkFrom.equals(chunkTo)) {
                     return;
+                } else {
+                    if(ChunkManager.isClaimed(chunkTo) && ChunkManager.isClaimed(chunkFrom)) {
+                        if(ChunkManager.getKingdomOwner(chunkFrom).equals(ChunkManager.getKingdomOwner(chunkTo))) {
+                            return;
+                        } else {
+                            player.sendMessage(QCChat.getPhrase("now_leaving_the_land_of") + ChunkManager.getKingdomOwner(chunkFrom).getName());
+                            player.sendMessage(QCChat.getPhrase("now_entering_the_land_of") + ChunkManager.getKingdomOwner(chunkTo).getName());
+                            return;
+                        }
+                    } else if(ChunkManager.isClaimed(chunkFrom)) {
+                        player.sendMessage(QCChat.getPhrase("now_leaving_the_land_of") + ChunkManager.getKingdomOwner(chunkFrom).getName());
+                        return;
+                    } else if(ChunkManager.isClaimed(chunkTo)) {
+                        player.sendMessage(QCChat.getPhrase("now_entering_the_land_of") + ChunkManager.getKingdomOwner(chunkTo).getName());
+                        return;
+                    } else {
+                        return;
+                    }
                 }
-            } else if(ChunkManager.isClaimed(event.getFrom().getChunk())) {
-                player.sendMessage(QCChat.getPhrase("now_leaving_the_land_of") + ChunkManager.getKingdomOwner(event.getFrom().getChunk()));
-            } else if(ChunkManager.isClaimed(event.getTo().getChunk())) {
-                player.sendMessage(QCChat.getPhrase("now_entering_the_land_of") + ChunkManager.getKingdomOwner(event.getTo().getChunk()));
-            } else {
-                return;
             }
-        }
+        }).execute();
     }
 }
