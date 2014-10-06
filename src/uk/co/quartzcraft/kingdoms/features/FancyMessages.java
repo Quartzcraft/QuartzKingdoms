4d6f896fe5217591980c1fb79571f5d75d63f774
ZeWildGuy <njb3112@iCloud.com>
2014-10-06 17:21:28 +1000

    declaredWar() and proposedAlly() fancy messages

--- src/uk/co/quartzcraft/kingdoms/features/FancyMessages.java
+++ src/uk/co/quartzcraft/kingdoms/features/FancyMessages.java
@@ -8,4 +8,26 @@ import static org.bukkit.ChatColor.RED;
 import static org.bukkit.ChatColor.UNDERLINE;
 
 public class FancyMessages {
+
+    public static String declaredWar(Player player, String kingdomName) {
+        return new FancyMessage("The kingdom ").color(GREEN)
+                .then(kingdomName).color(WHITE).style(ITALIC)
+                .then(" has declared ").color(GREEN)
+                .then("war").color(DARK_RED)
+                .then(" against your kingdom. ").color(GREEN)
+                .then("You can ignore this with no consequences or you can ").color(GREEN)
+                .then("declare war.").color(DARK_RED).style(UNDERLINE).suggest("/kingdom war " + kingdomName).tooltip("Declares war against the specified kingdom.")
+                .toJSONString();
+    }
+
+    public static String proposedAlly(Player player, String kingdomName) {
+        return new FancyMessage("The kingdom ").color(GREEN)
+                .then(kingdomName).color(WHITE).style(ITALIC)
+                .then(" has offered to become an ").color(GREEN)
+                .then("ally").color(DARK_GREEN)
+                .then(" with your kingdom. ").color(GREEN)
+                .then("You can ignore this with no consequences or you can ").color(GREEN)
+                .then("accept the offer.").color(DARK_GREEN).style(UNDERLINE).suggest("/kingdom ally " + kingdomName).tooltip("Allies with the specified kingdom.")
+                .toJSONString();
+    }
 }
