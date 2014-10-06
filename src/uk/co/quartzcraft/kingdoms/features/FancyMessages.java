03d6c9bc53ff22efc66f5cca8f73d725dc3a8b53
ZeWildGuy <njb3112@iCloud.com>
2014-10-06 17:21:08 +1000

    FancyMessages class

---
+++ src/uk/co/quartzcraft/kingdoms/features/FancyMessages.java
@@ -0,0 +1,11 @@
+package uk.co.quartzcraft.kingdoms.features;
+
+import org.bukkit.entity.Player;
+import uk.co.quartzcraft.core.systems.fancymessage.FancyMessage;
+
+import static org.bukkit.ChatColor.*;
+import static org.bukkit.ChatColor.RED;
+import static org.bukkit.ChatColor.UNDERLINE;
+
+public class FancyMessages {
+}
