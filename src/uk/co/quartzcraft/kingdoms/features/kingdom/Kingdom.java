12563858ab2f7b29886a4a6a8aab683068ce3d0f
ZeWildGuy <njb3112@iCloud.com>
2014-12-28 09:39:56 +1000

    Querying war as sec kingdom

--- src/uk/co/quartzcraft/kingdoms/features/kingdom/Kingdom.java
+++ src/uk/co/quartzcraft/kingdoms/features/kingdom/Kingdom.java
@@ -470,6 +470,12 @@ public class Kingdom {
             s.setInt(1, this.id);
             s.setInt(2, relatingKingdom.getID());
             ResultSet res = s.executeQuery();
+
+            java.sql.PreparedStatement s1 = QuartzKingdoms.DBKing.prepareStatement("SELECT * FROM relationships WHERE kingdom_id=? AND sec_kingdom_id=?;");
+            s1.setInt(1, relatingKingdom.getID());
+            s1.setInt(2, this.id);
+            ResultSet res1 = s1.executeQuery();
+
             if(isEnemy(relatingKingdom)) {
                 return 3;
             } else if(res.next()) {
