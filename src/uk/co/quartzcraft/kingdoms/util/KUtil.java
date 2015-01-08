ffac737302d3fbac238b285d62cb3adb753d6d4b
ZeWildGuy <njb3112@iCloud.com>
2015-01-09 09:29:17 +1000

    sendMsg, sendPhrase and broadcastMsg with kingdoms prefix

--- src/uk/co/quartzcraft/kingdoms/util/KUtil.java
+++ src/uk/co/quartzcraft/kingdoms/util/KUtil.java
@@ -1,5 +1,9 @@
 package uk.co.quartzcraft.kingdoms.util;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import uk.co.quartzcraft.core.systems.chat.QCChat;
+import uk.co.quartzcraft.core.util.Util;
 import uk.co.quartzcraft.kingdoms.QuartzKingdoms;
 
 import java.util.logging.Level;
@@ -13,5 +17,20 @@ public class KUtil {
 
     public static void log(Level level, String message) {
         QuartzKingdoms.log.log(level, message);
+    }
+
+    public static void sendMsg(Player player, String msg) {
+        String finalmsg = Util.colour(msg);
+        player.sendMessage(QCChat.getPhrase("kingdoms_prefix") + finalmsg);
+    }
+
+    public static void sendPhrase(Player player, String phrase) {
+        String finalmsg = QCChat.getPhrase(phrase);
+        player.sendMessage(QCChat.getPhrase("kingdoms_prefix") + finalmsg);
+    }
+
+    public static void broadcastMsg(String msg) {
+        String finalmsg = Util.colour(msg);
+        Bukkit.broadcastMessage(QCChat.getPhrase("kingdoms_prefix") + finalmsg);
     }
 }
