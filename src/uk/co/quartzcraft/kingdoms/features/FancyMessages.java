2874c9d316c2106d765b5556b7d924cc1b91cb63
ZeWildGuy <njb3112@iCloud.com>
2015-09-27 22:05:27 +1000

    No longer needing proposedAlly fancy message

--- src/uk/co/quartzcraft/kingdoms/features/FancyMessages.java
+++ src/uk/co/quartzcraft/kingdoms/features/FancyMessages.java
@@ -9,15 +9,4 @@ import static org.bukkit.ChatColor.UNDERLINE;
 
 public class FancyMessages {
 
-
-    public static String proposedAlly(Player player, String kingdomName) {
-        return new FancyMessage("The kingdom ").color(GREEN)
-                .then(kingdomName).color(WHITE).style(ITALIC)
-                .then(" has offered to become an ").color(GREEN)
-                .then("ally").color(DARK_GREEN)
-                .then(" with your kingdom. ").color(GREEN)
-                .then("You can ignore this with no consequences or you can ").color(GREEN)
-                .then("accept the offer.").color(DARK_GREEN).style(UNDERLINE).suggest("/kingdom ally " + kingdomName).tooltip("Allies with the specified kingdom.")
-                .toJSONString();
-    }
 }
