110125814ed81423db06fe07d9e3cff1739660ec
ZeWildGuy <njb3112@iCloud.com>
2016-04-09 18:19:21 +1000

    Moving phrases to their own class

--- src/uk/co/quartzcraft/kingdoms/features/chat/ChatPhrases.java
+++ src/uk/co/quartzcraft/kingdoms/features/chat/ChatPhrases.java
@@ -1,66 +1,10 @@
-package uk.co.quartzcraft.kingdoms;
+package uk.co.quartzcraft.kingdoms.features.chat;
 
-import java.sql.Connection;
-import java.util.Arrays;
-import java.util.logging.Logger;
-
-import com.sun.tools.javac.util.List;
-
-import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import uk.co.quartzcraft.core.QuartzCore;
 import uk.co.quartzcraft.core.systems.chat.QCChat;
-import uk.co.quartzcraft.core.command.framework.QCommandFramework;
-import uk.co.quartzcraft.core.database.MySQL;
-import uk.co.quartzcraft.core.command.framework.QCommand;
-import uk.co.quartzcraft.kingdoms.command.*;
-import uk.co.quartzcraft.kingdoms.listeners.*; 
 
-public class QuartzKingdoms extends JavaPlugin {
-	
-	public static Plugin plugin;
-    public static Plugin core;
-	
-	public final Logger logg = Logger.getLogger("Minecraft");
-    public static final Logger log = Logger.getLogger("Minecraft");
-	
-	public static String releaseVersion = QuartzCore.version;
-    public static String version;
+public class ChatPhrases {
 
-	public static Connection DBKing = null;
-	public static MySQL MySQLking = null;
-
-    public QCommandFramework commandFramework;
-	
-	@Override
-	public void onDisable() {
-				
-    	//Shutdown notice
-		log.info("[QK] The QuartzKingdoms Plugin has been disabled!");
-	}
-	
-	@Override
-	public void onEnable() {
-
-        version = this.getDescription().getVersion();
-        plugin = this;
-        core = QuartzCore.plugin;
-		
-		log.info("[QK] Running plugin configuration");
-		this.saveDefaultConfig();
-		
-		String SQLKingHost = this.getConfig().getString("database.kingdoms.host");
-		String SQLKingDatabase = this.getConfig().getString("database.kingdoms.database");
-		String SQLKingUser = this.getConfig().getString("database.kingdoms.username");
-		String SQLKingPassword = this.getConfig().getString("database.kingdoms.password");
-		MySQLking = new MySQL(plugin, SQLKingHost, "3306", SQLKingDatabase, SQLKingUser, SQLKingPassword);
-		
-		//Phrases
-		log.info("[QK][STARTUP]Creating Phrases");
-
+    public static void phrases() {
         QCChat.addPhrase("kingdoms_prefix", "&8[&aKingdoms&8]");
 
         QCChat.addPhrase("you_do_not_have_permission_to_build_here", "&cYou do not have permission to build here!");
@@ -148,32 +92,5 @@ public class QuartzKingdoms extends JavaPlugin {
         QCChat.addPhrase("kingdom_is_allied_with_your_kingdom", "&aThis kingdom is allied with your kingdom!");
 
         QCChat.addPhrase("could_not_create_kingdoms_player", "&cYour player data could not be added to the QuartzKingdoms database!");
-		
-		//Database
-		log.info("[QK][STARTUP] Connecting to Database");
-		DBKing = MySQLking.openConnection();
-		
-		//Listeners
-		log.info("[QK][STARTUP] Registering Listeners");
-		new BlockListener(this);
-		new PlayerListener(this);
-        new PInfoListener(this);
-		
-		//Commands
-		log.info("[QK][STARTUP] Registering Commands");
-        commandFramework = new QCommandFramework(this);
-        commandFramework.registerCommands(new CommandKingdom(this, core));
-        commandFramework.registerCommands(new CommandKAdmin(this, core));
-	   	
-        //Startup notice
-		log.info("[QK] The QuartzKingdoms Plugin has been enabled!");
-		log.info("[QK] QuartzKingdoms version " + version + " compiled with QuartzCore version " + releaseVersion);
-        //log.info("[QK] QuartzKingdoms version " + version + " compiled with QuartzCore version " + QuartzCore.getVersion());
-	}
-
-    @Override
-    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-        return commandFramework.handleCommand(sender, label, command, args);
     }
-
 }
