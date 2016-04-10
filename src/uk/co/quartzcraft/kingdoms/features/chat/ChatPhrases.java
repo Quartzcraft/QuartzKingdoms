3b6fd918e051c397bad78afbca6a704c96c13911
ZeWildGuy <njb3112@iCloud.com>
2016-04-10 10:22:12 +1000

    Added another phrase

--- src/uk/co/quartzcraft/kingdoms/features/chat/ChatPhrases.java
+++ src/uk/co/quartzcraft/kingdoms/features/chat/ChatPhrases.java
@@ -71,6 +71,8 @@ public class ChatPhrases {
 
         QCChat.addPhrase("invited_player_to_kingdom", "&aYou have successfully invited that player to join your kingdom!");
         QCChat.addPhrase("failed_to_invited_player_to_kingdom", "&cPlayer could not be invited to your kingdom! Please try again.");
+        QCChat.addPhrase("you_have_not_been_invited_to_this_kingdom", "&cYou have not been invited to join this kingdom!");
+
         QCChat.addPhrase("kingdom_already_open", " &cThe kingdom is already open!");
         QCChat.addPhrase("kingdom_now_open", " &aYour kingdom is now open!");
         QCChat.addPhrase("failed_open_kingdom", " &cFailed to open the kingdom!");
